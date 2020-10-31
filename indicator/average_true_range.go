184e061fc65a628beed8e03f73d1f06a70aef1e6
candfan <118430919+candfan@users.noreply.github.com>
2020-10-31 21:02:10 +0000

    Update WebGrab++.config.xml

--- indicator/average_true_range.go
+++ indicator/average_true_range.go
@@ -8,3 +8,14 @@ import (
 )
 
 // AverageTrueRange represents indicator to calculate Average True Range (ATR)
+// https://en.wikipedia.org/wiki/Average_true_range
+type AverageTrueRange struct {
+	series       *timeseries.TimeSeries
+	period       int
+	cache        map[int]float64
+	lastComputed int
+}
+
+func NewAverageTrueRange(series *timeseries.TimeSeries, period int) Indicator {
+	return &AverageTrueRange{series, period, make(map[int]float64), period - 1}
+}
