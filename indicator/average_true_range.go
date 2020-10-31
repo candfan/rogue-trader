d1373b55bac622ee1972cf2cd30a9ac2e68d41ed
candfan <118430919+candfan@users.noreply.github.com>
2020-10-31 14:41:41 +0000

    Set theme jekyll-theme-leap-day

---
+++ indicator/average_true_range.go
@@ -0,0 +1,10 @@
+
+package indicator
+
+import (
+	"math"
+
+	"github.com/evsamsonov/trading-timeseries/timeseries"
+)
+
+// AverageTrueRange represents indicator to calculate Average True Range (ATR)
