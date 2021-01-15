b0fd04365c0fab2098307dba4e423698427a7cb2
candfan <118430919+candfan@users.noreply.github.com>
2021-01-15 13:52:49 +0000

    reorganized

--- indicator/average_true_range_test.go
+++ indicator/average_true_range_test.go
@@ -23,4 +23,4 @@ func TestAverageTrueRange_Calculate(t *testing.T) {
 		atr := atrIndicator.Calculate(test.index)
 		expectedAtr := test.expected
 		if math.Abs(atr-expectedAtr) > float64EqualityThreshold {
-			t.Errorf("Result %f not equals expe
+			t.Errorf("Result %f not equals expected %f", atr, ex
