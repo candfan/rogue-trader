1e6515eedc0267279f4e54dc9de14abaea8531de
candfan <118430919+candfan@users.noreply.github.com>
2021-02-18 20:36:53 +0000

    Update resource strings

--- indicator/average_volume.go
+++ indicator/average_volume.go
@@ -3,4 +3,5 @@ package indicator
 import "github.com/evsamsonov/trading-timeseries/timeseries"
 
 // AverageVolume is a indicator to calculate average volume
-// for given smoothInterval using simple movin
+// for given smoothInterval using simple moving average
+type A
