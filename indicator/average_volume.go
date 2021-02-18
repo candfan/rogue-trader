f4f814974b4e7a63535b0ec7730feb8b1c8436a8
candfan <118430919+candfan@users.noreply.github.com>
2021-02-18 14:29:43 +0000

    drm/nouveau: fix blank LVDS screen regression on pre-nv50 cards

--- indicator/average_volume.go
+++ indicator/average_volume.go
@@ -3,4 +3,4 @@ package indicator
 import "github.com/evsamsonov/trading-timeseries/timeseries"
 
 // AverageVolume is a indicator to calculate average volume
-// for given smoothInterval using s
+// for given smoothInterval using simple movin
