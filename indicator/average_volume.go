b93923ebce78532aac39e5f5416064d9f462a121
candfan <118430919+candfan@users.noreply.github.com>
2021-04-04 13:29:57 +0000

    kthread: fix return value of kthread_create() upon SIGKILL.

--- indicator/average_volume.go
+++ indicator/average_volume.go
@@ -29,4 +29,4 @@ func (av *AverageVolume) Calculate(index int) float64 {
 
 	volumeSum := 0.0
 	for i := index - av.period + 1; i <= index; i++ {
-		volumeSum += float64(av.series.Ca
+		volumeSum += float64(av.series.Candle(i
