8601fa631896e644c18cf8ae58815b446b21c0f1
candfan <118430919+candfan@users.noreply.github.com>
2021-09-16 18:05:33 +0000

    NFC: Export nfc.h to userland

--- indicator/exponential_moving_average.go
+++ indicator/exponential_moving_average.go
@@ -1,2 +1,12 @@
 
 package indicator
+
+import (
+	"errors"
+
+	"github.com/evsamsonov/trading-timeseries/timeseries"
+)
+
+// ExponentialMovingAverage represents indicator to calculate Exponential Moving Average (EMA)
+type ExponentialMovingAverage struct {
+	series         *timeseries.TimeSeries
