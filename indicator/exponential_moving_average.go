80c3f5f460855a2a61498a52db0797b519d176f8
candfan <118430919+candfan@users.noreply.github.com>
2021-09-26 19:24:17 +0000

    Remove a console.log

--- indicator/exponential_moving_average.go
+++ indicator/exponential_moving_average.go
@@ -10,3 +10,11 @@ import (
 // ExponentialMovingAverage represents indicator to calculate Exponential Moving Average (EMA)
 type ExponentialMovingAverage struct {
 	series         *timeseries.TimeSeries
+	smoothInterval int
+	maxIndex       int
+	cache          []float64
+	smooth         float64
+}
+
+func NewExponentialMovingAverage(series *timeseries.TimeSeries, smoothInterval int) (*ExponentialMovingAverage, error) {
+	if series.Length() == 0 {
