ae2d07837dbce01ee606244bab3a245f335f806a
candfan <118430919+candfan@users.noreply.github.com>
2022-03-20 22:45:09 +0000

    fix load

--- indicator/exponential_moving_average_test.go
+++ indicator/exponential_moving_average_test.go
@@ -19,4 +19,4 @@ func TestExponentialMovingAverage_Calculate(t *testing.T) {
 		expected       float64
 	}{
 		{name: "not enough data", smoothInterval: 3, index: 0, expected: 22.1044},
-		{name: "smoothInterval=3,i
+		{name: "smoothInterval=3,index=2"
