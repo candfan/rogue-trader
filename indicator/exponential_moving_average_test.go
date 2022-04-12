242f5bb2fba68f148e3cace92c21a54ed835bf40
candfan <118430919+candfan@users.noreply.github.com>
2022-04-12 18:35:14 +0000

    Update Gitignore

--- indicator/exponential_moving_average_test.go
+++ indicator/exponential_moving_average_test.go
@@ -24,4 +24,5 @@ func TestExponentialMovingAverage_Calculate(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			indicator, err := NewExponentialMovingAverage(series, test.smoothInter
+			indicator, err := NewExponentialMovingAverage(series, test.smoothInterval)
+			
