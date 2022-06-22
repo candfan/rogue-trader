75d223f898faac1609eee452e443c920480825cb
candfan <118430919+candfan@users.noreply.github.com>
2022-06-22 05:34:48 +0000

    kj

--- indicator/exponential_moving_average_test.go
+++ indicator/exponential_moving_average_test.go
@@ -60,4 +60,5 @@ func BenchmarkExponentialMovingAverage_Calculate(b *testing.B) {
 	b.ResetTimer()
 	var wg sync.WaitGroup
 	for i := 0; i < b.N; i++ {
-		for j := 0; j < 4; j++ 
+		for j := 0; j < 4; j++ {
+			wg.A
