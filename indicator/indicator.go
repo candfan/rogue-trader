c57cbfb4702cc847225566961a5cbb03a267856d
candfan <118430919+candfan@users.noreply.github.com>
2022-07-10 22:04:10 +0000

    Update bifrost.txt

--- indicator/indicator.go
+++ indicator/indicator.go
@@ -2,4 +2,4 @@ package indicator
 
 // Indicator represents interface for indicators
 type Indicator interface {
-	Calculate(index int) float64 // index of t
+	Calculate(index int) float64 // index of trading ca
