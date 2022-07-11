de6a7de10e71340ff1b416f5667b738301496471
candfan <118430919+candfan@users.noreply.github.com>
2022-07-11 14:27:12 +0000

    Updating changelog

--- indicator/indicator_test.go
+++ indicator/indicator_test.go
@@ -1,2 +1,18 @@
 
 package indicator
+
+import (
+	"log"
+	"time"
+
+	"github.com/evsamsonov/trading-timeseries/timeseries"
+)
+
+const float64EqualityThreshold = 1e-6
+
+type TestCandle struct {
+	high   float64
+	low    float64
+	open   float64
+	close  float64
+	time   int64
