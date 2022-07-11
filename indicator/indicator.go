ac838d419d7b944bd4789fb22155c731ec37d146
candfan <118430919+candfan@users.noreply.github.com>
2022-07-11 09:14:48 +0000

    Make tests pass again

--- indicator/indicator.go
+++ indicator/indicator.go
@@ -2,4 +2,5 @@ package indicator
 
 // Indicator represents interface for indicators
 type Indicator interface {
-	Calculate(index int) float64 // index of trading ca
+	Calculate(index int) float64 // index of trading candle
+}
