fa709962fdae70c74e08f48f051c8714fcf40d9f
candfan <118430919+candfan@users.noreply.github.com>
2022-07-23 15:43:40 +0000

    generated: Run hack/lint-dependencies.sh and hack/update-vendor.sh

--- indicator/trend.go
+++ indicator/trend.go
@@ -1,2 +1,15 @@
 
 package indicator
+
+import "math"
+
+const (
+	FlatTrend float64 = 0
+	UpTrend   float64 = 1
+	DownTrend float64 = -1
+)
+
+// Trend returns a trend direction.
+// It bases on fast (with shorter period) and slow EMA.
+// flatMaxDiff allows setting max difference between
+// fast and slow EMA when Calculate returns the flat.
