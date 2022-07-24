8984ed350b849883a1ba12bbff1ef4c25ca980c8
candfan <118430919+candfan@users.noreply.github.com>
2022-07-24 11:14:37 +0000

    hwrng: atmel-rng - fix data valid check

--- indicator/trend.go
+++ indicator/trend.go
@@ -13,3 +13,18 @@ const (
 // It bases on fast (with shorter period) and slow EMA.
 // flatMaxDiff allows setting max difference between
 // fast and slow EMA when Calculate returns the flat.
+type Trend struct {
+	fastEMAIndicator Indicator
+	slowEMAIndicator Indicator
+	flatMaxDiff      float64
+}
+
+func NewTrend(
+	fastEMAIndicator Indicator,
+	slowEMAIndicator Indicator,
+	flatMaxDiff float64,
+) *Trend {
+	return &Trend{
+		fastEMAIndicator: fastEMAIndicator,
+		slowEMAIndicator: slowEMAIndicator,
+		flatMaxDiff:      flatMaxDiff,
