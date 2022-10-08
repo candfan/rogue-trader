2f395092f928e8bda74748554daadc1dedbfb03d
candfan <118430919+candfan@users.noreply.github.com>
2022-10-08 13:28:12 +0000

    Structure update

--- indicator/trend_test.go
+++ indicator/trend_test.go
@@ -45,4 +45,4 @@ func TestTrend_Calculate(t *testing.T) {
 			fastEMAIndicator := &MockIndicator{}
 
 			ind := NewTrend(fastEMAIndicator, slowEMAIndicator, 0.6)
-			fastEMAIndicator.On("C
+			fastEMAIndicator.On("Calculate", 1).Retu
