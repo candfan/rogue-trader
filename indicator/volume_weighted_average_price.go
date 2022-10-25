46970e1d7888b44e11749ec6e5329103ca2373a3
candfan <118430919+candfan@users.noreply.github.com>
2022-10-25 05:40:43 +0000

    example fixed

--- indicator/volume_weighted_average_price.go
+++ indicator/volume_weighted_average_price.go
@@ -11,3 +11,14 @@ import (
 const oneDay = time.Hour * 24
 
 // VolumeWeightedAveragePrice represents indicator to calculate volume-weighted average price (VWAP).
+// More details https://en.wikipedia.org/wiki/Volume-weighted_average_price
+type VolumeWeightedAveragePrice struct {
+	series *timeseries.TimeSeries
+	cache  *vwapCache
+}
+
+// NewVolumeWeightedAveragePrice creates VolumeWeightedAveragePrice
+func NewVolumeWeightedAveragePrice(series *timeseries.TimeSeries) *VolumeWeightedAveragePrice {
+	return &VolumeWeightedAveragePrice{
+		series: series,
+		cache:  newVwapCache(),
