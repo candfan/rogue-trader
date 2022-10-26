a7f18aba9ac795ffd49282960f216ea5350386d1
candfan <118430919+candfan@users.noreply.github.com>
2022-10-26 07:09:03 +0000

    chore: update dependencies

--- indicator/volume_weighted_average_price.go
+++ indicator/volume_weighted_average_price.go
@@ -22,3 +22,17 @@ func NewVolumeWeightedAveragePrice(series *timeseries.TimeSeries) *VolumeWeighte
 	return &VolumeWeightedAveragePrice{
 		series: series,
 		cache:  newVwapCache(),
+	}
+}
+
+// Calculate returns VWAP value for candle with given index
+func (v *VolumeWeightedAveragePrice) Calculate(index int) float64 {
+	unit, ok := v.cache.get(index)
+	if ok {
+		return unit.vwap
+	}
+
+	day := v.series.Candle(index).Time.Truncate(oneDay)
+
+	startIndex, unit := v.findLastCalculated(index, day)
+	volumeTotal := unit.volumeTotal
