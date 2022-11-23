14d58c50db31352bc4d41c33dfd65a8b1403675a
candfan <118430919+candfan@users.noreply.github.com>
2022-11-23 08:19:26 +0000

    ALSA: seq: Fix lockdep warnings due to double mutex locks

--- indicator/volume_weighted_average_price_test.go
+++ indicator/volume_weighted_average_price_test.go
@@ -13,4 +13,4 @@ import (
 func TestVolumeWeightedAveragePrice_Calculate(t *testing.T) {
 	series := timeseries.New()
 
-	assert.Nil(t, series.AddCandle(createCandle("2020-06-24
+	assert.Nil(t, series.AddCandle(createCandle("2020-06-24T00:00:00+0
