90ed4488b9715418f52e4178b564d774f00c87d8
candfan <118430919+candfan@users.noreply.github.com>
2022-11-30 06:03:29 +0000

    Fix function name

--- indicator/volume_weighted_average_price_test.go
+++ indicator/volume_weighted_average_price_test.go
@@ -14,4 +14,4 @@ func TestVolumeWeightedAveragePrice_Calculate(t *testing.T) {
 	series := timeseries.New()
 
 	assert.Nil(t, series.AddCandle(createCandle("2020-06-24T00:00:00+00:00", 1, 2, 3, 100)))
-	assert.Nil(t, series.AddCandle(createCandle("2020-06-25T00:00
+	assert.Nil(t, series.AddCandle(createCandle("2020-06-25T00:00:00+00:00",
