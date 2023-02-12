80416b91a2027714c77bacc9c647f799cceec6f4
candfan <118430919+candfan@users.noreply.github.com>
2023-02-12 14:18:27 +0000

    added note

--- indicator/volume_weighted_average_price_test.go
+++ indicator/volume_weighted_average_price_test.go
@@ -38,4 +38,4 @@ func TestVolumeWeightedAveragePrice_Calculate(t *testing.T) {
 }
 
 func createCandle(date string, high, low, close float64, volume int64) *timeseries.Candle {
-	candle := timese
+	candle := timeseries.NewCand
